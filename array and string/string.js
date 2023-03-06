12782bceb6c9f44cb64ec3643b4725fb032c38f5
Ramla Jama <75176930+ramla199@users.noreply.github.com>
2023-03-06 00:46:29 +0000

    "string note 3"

--- array and string/string.js
+++ array and string/string.js
@@ -1,5 +1,15 @@
 // A JavaScript string is zero or more characters written inside quotes.
-// let text = "John Doe";
+//1# let text = "John Doe";
+//2# let num = 5;
+// let str= num.toString()
+//3# Back-Tics Syntax
+//// Template Literals use back-ticks (``) rather than the quotes ("") to define a string:
+//Synonyms:
+// Template Literals
+// Template Strings
+// String Templates
+// Back-Tics Syntax
+// let text = `Hello World!`;
 
 // Escape Character
 // The backslash (\) escape character turns special characters into string characters:
@@ -55,5 +65,53 @@
 // String concat()
 // String trim()// String trimStart()// String trimEnd()
 // String padStart()// String padEnd()
-// String charAt()// String charCodeAt()
 // String split()
+
+// Extracting String Characters
+// There are 3 methods for extracting string characters:
+
+// charAt(position)
+// charCodeAt(position)
+// Property access [ ]
+
+// JavaScript Search Methods
+// String indexOf()
+// String lastIndexOf()
+// String search()
+// String match()
+// String matchAll()
+// String includes()
+// String startsWith()
+// String endsWith()
+
+
+// charAt()	Returns the character at a specified index (position)
+// charCodeAt()	Returns the Unicode of the character at a specified index
+// concat()	Returns two or more joined strings
+// constructor	Returns the string's constructor function
+// endsWith()	Returns if a string ends with a specified value
+// fromCharCode()	Returns Unicode values as characters
+// includes()	Returns if a string contains a specified value
+// indexOf()	Returns the index (position) of the first occurrence of a value in a string
+// lastIndexOf()	Returns the index (position) of the last occurrence of a value in a string
+// length	Returns the length of a string
+// localeCompare()	Compares two strings in the current locale
+// match()	Searches a string for a value, or a regular expression, and returns the matches
+// prototype	Allows you to add properties and methods to an object
+// repeat()	Returns a new string with a number of copies of a string
+// replace()	Searches a string for a value, or a regular expression, and returns a string where the values are replaced
+// search()	Searches a string for a value, or regular expression, and returns the index (position) of the match
+// slice()	Extracts a part of a string and returns a new string
+// split()	Splits a string into an array of substrings
+// startsWith()	Checks whether a string begins with specified characters
+// substr()	Extracts a number of characters from a string, from a start index (position)
+// substring()	Extracts characters from a string, between two specified indices (positions)
+// toLocaleLowerCase()	Returns a string converted to lowercase letters, using the host's locale
+// toLocaleUpperCase()	Returns a string converted to uppercase letters, using the host's locale
+// toLowerCase()	Returns a string converted to lowercase letters
+// toString()	Returns a string or a string object as a string
+// toUpperCase()	Returns a string converted to uppercase letters
+// trim()	Returns a string with removed whitespaces
+// trimEnd()	Returns a string with removed whitespaces from the end
+// trimStart()	Returns a string with removed whitespaces from the start
+// valueOf()	Returns the primitive value of a string or a string object
